527c28bfc17fe019eafd0aa5ec6efa76c5293d1e
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-06-12 11:57:36 -0300

    :sparkles: Feat: filtrando dados

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
-import { IListItens } from '../../interface/IListItens.interface';
+import { IListItens as IListItems } from '../../interface/IListItens.interface';
 import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 
 @Component({
@@ -16,19 +16,33 @@ import { InputListItemComponent } from '../../components/input-list-item/input-l
 export class ListComponent {
   public addItem = signal(true);
 
-  #setListItens = signal<IListItens[]>(this.#parseItens());
+  #setListItens = signal<IListItems[]>(this.#parseItens());
   public getListItems = this.#setListItens.asReadonly();
 
   #parseItens() {
     return JSON.parse(localStorage.getItem('@my-list') || '[]');
   }
 
-  public getInputAndAddItem(value: IListItens) {
+  public getInputAndAddItem(value: IListItems) {
     localStorage.setItem(
       '@my-list', JSON.stringify([ ...this.#setListItens(), value ])
     );
 
     return this.#setListItens.set(this.#parseItens());
+  }
+
+  public listItemsStage(value: 'pending' | 'completed') {
+    return this.getListItems().filter((res: IListItems) => {
+      if(value === 'pending') {
+        return !res.checked;
+      }
+
+      if(value === 'completed') {
+        return res.checked;
+      }
+
+      return res;
+    })
   }
 
   public deleteAllItems() {
